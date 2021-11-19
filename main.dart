9187472f26569570307b31a42fea34be0eee9336
thomasjerryarackal <42430083+thomasjerryarackal@users.noreply.github.com>
2021-11-19 11:06:33 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  Home(),
+
+  ));
+}
+//start
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late VideoPlayerController _controller;
+
+//video start
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    _controller = VideoPlayerController.asset("video/k.mp4")
+      ..initialize().then((_) {
+        // Once the video has been loaded we play the video and set looping to true.
+        _controller.play();
+        _controller.setLooping(true);
+        // Ensure the first frame is shown after the video is initialized.
+        setState(() {
+          _controller.play();
+
+        });
+      });
+  }
+  @override
+  void dispose() {
+    super.dispose();
+      _controller.dispose();
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Stack(
+            children: [
+              //  Add a SizedBox to contain our video.
+              SizedBox.expand(
+                child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: SizedBox(
+                    
+                    child: VideoPlayer(_controller),
+                  ),
+                ),
+              ),
+              Name(),
+            ],
+          ),
+        ),
+
+      ),
+    );
+
+  }
+}
+
+class Name extends StatelessWidget {
+  const Name({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("YoU ArE iN tHe LooP.....",
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 20
+        ),
+        )
+      ],
+    );
+  }
+}
+
+
+
